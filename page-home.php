ed182e59c16bc9388c76cefc37d2bcebc042ed16
DiaMedical USA User <benz_rob@yahoo.com>
2018-02-14 08:13:14 -0500

    cleaned up wpconfig a little bit

--- page-home.php
+++ page-home.php
@@ -208,30 +208,40 @@ $features_section_body	= get_field('features_section_body');
 </section><!-- course-features -->
 
 
+<?php
+$project_feature_title	= get_field('project_feature_title');
+$project_feature_body	  = get_field('project_feature_body');
+?>
+
 <!-- PROJECT FEATURES
 ================================================== -->
 <section id="project-features">
 <div class="container">
 
-  <h2>Final Project Features</h2>
-  <p class="lead">Throughout this entire course, you work towards building an incredibly beautiful website. Want to see the website <strong>you</strong> are going to build? <em>You're looking at it!</em> The website you're using right now is the website you will have built entirely by yourself, by the end of this course.</p>
+  <h2><?php echo $project_feature_title; ?></h2>
+  <p class="lead"><?php echo $project_feature_body; ?></p>
 
   <div class="row">
+
+    <?php
+    $loop = new WP_Query(
+      array(
+        'post_type' => 'project_feature',
+        'orderby'   => 'post_id',
+        'order'     => 'ASC'
+      )
+    );
+    while( $loop->have_posts() ) : $loop->the_post(); ?>
+
     <div class="col-sm-4">
-      <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-design.png" alt="Design">
-      <h3>Sexy &amp; Modern Design</h3>
-      <p>You get to work with a modern, professional quality design &amp; layout.</p>
-    </div><!-- col -->
-    <div class="col-sm-4">
-      <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-code.png" alt="Code">
-      <h3>Quality HTML5 &amp; CSS3</h3>
-      <p>You'll learn how hand-craft a stunning website with valid, semantic and beautiful HTML5 &amp; CSS3.</p>
-    </div><!-- col -->
-    <div class="col-sm-4">
-      <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-cms.png" alt="CMS">
-      <h3>Easy-to-use CMS</h3>
-      <p>Allow your clients to easily update their websites by converting your static websites to dynamic websites, using WordPress.</p>
-    </div><!-- col -->
+
+      <?php if ( has_post_thumbnail() ) the_post_thumbnail(); ?>
+
+      <h3><?php the_title(); ?></h3>
+      <p><?php the_content(); ?></p>
+    </div><!-- end col -->
+
+    <?php endwhile; wp_reset_query(); ?>
 
   </div><!-- row -->
 
