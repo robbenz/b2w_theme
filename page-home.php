9b4c3d2f5263d09753660cb4a3af8fabe20a8c3f
DiaMedical USA User <dmuser@Robss-iMac.local>
2018-02-14 07:29:28 -0500

    finished next section, standard wp while loop->the_post and custom post type UI build

--- page-home.php
+++ page-home.php
@@ -152,50 +152,59 @@ $who_section_body		  = get_field('who_section_body');
 	</div><!-- container -->
 </section><!-- who-benefits -->
 
+
+<?php
+$features_section_image	= get_field('features_section_image');
+$features_section_title	= get_field('features_section_title');
+$features_section_body	= get_field('features_section_body');
+?>
+
+
 <!-- COURSE FEATURES
 ================================================== -->
 <section id="course-features">
 <div class="container">
 
   <div class="section-header">
-    <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-rocket.png" alt="Rocket">
-    <h2>Course Features</h2>
+
+  <!-- If user uploaded an image -->
+  <?php if( !empty($features_section_image) ) : ?>
+
+  <img src="<?php echo $features_section_image['url']; ?>" alt="<?php echo $features_section_image['alt']; ?>">
+
+  <?php endif; ?>
+
+    <h2><?php echo $features_section_title; ?></h2>
+
+    <!-- If user added body text -->
+    <?php if( !empty($features_section_body) ) : ?>
+    <p class="lead"><?php echo $features_section_body; ?></p>
+    <?php endif; ?>
+
   </div><!-- section-header -->
 
   <div class="row">
 
-    <div class="col-sm-2">
-      <i class="ci ci-computer"></i>
-      <h4>Lifetime access to 80+ lectures</h4>
-    </div><!-- end col -->
+    <?php
+    $loop = new WP_Query(
+      array(
+        'post_type' => 'course_feature',
+        'orderby'   => 'post_id',
+        'order'     => 'ASC'
+      )
+    );
+    while( $loop->have_posts() ) : $loop->the_post();
+    ?>
 
     <div class="col-sm-2">
-      <i class="ci ci-watch"></i>
-      <h4>10+ hours of HD video content</h4>
+      <i class="<?php the_field('course_feature_icon'); ?>"></i>
+      <h4><?php the_title(); ?></h4>
     </div><!-- end col -->
 
-    <div class="col-sm-2">
-      <i class="ci ci-calendar"></i>
-      <h4>30-day money back guarantee</h4>
-    </div><!-- end col -->
+    <?php endwhile; wp_reset_query(); ?>
 
-    <div class="col-sm-2">
-      <i class="ci ci-community"></i>
-      <h4>Access to a community of like-minded students</h4>
-    </div><!-- end col -->
-
-    <div class="col-sm-2">
-      <i class="ci ci-instructor"></i>
-      <h4>Direct access to the instructor</h4>
-    </div><!-- end col -->
-
-    <div class="col-sm-2">
-      <i class="ci ci-device"></i>
-      <h4>Accessible content on your mobile devices</h4>
-    </div><!-- end col -->
-
-  </div><!-- row -->
-</div><!-- container -->
+    </div><!-- row -->
+  </div><!-- container -->
 </section><!-- course-features -->
 
 
